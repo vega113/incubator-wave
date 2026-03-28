f8dba3ab13b5b17f8c67c39636ce3736068d047b
Yuri Z. <vega113@gmail.com>
2026-03-28 08:07:59 +0300

    fix: wire OT search snapshots into live subscriptions (#407)
    * Wire OT search snapshots into live subscriptions
    
    * fix: canonicalize bootstrap live search snapshots
    
    * fix: remove duplicated search default constant
    
    * fix: stabilize live ot search subscriptions
    
    * test: add servlet test license header
    
    * fix: gate ot bootstrap and serialize snapshot updates
    
    * fix: move search widget query helper to shared package
    
    * fix: add release id for realtime search changelog

--- wave/src/test/java/org/waveprotocol/box/server/waveserver/search/SearchWidgetQueryStateTest.java
+++ wave/src/test/java/org/waveprotocol/box/server/waveserver/search/SearchWidgetQueryStateTest.java
@@ -2,17 +2,17 @@ package org.waveprotocol.box.server.waveserver.search;
 
 import junit.framework.TestCase;
 
-import org.waveprotocol.box.webclient.search.SearchWidgetQueryState;
+import org.waveprotocol.box.search.SearchWidgetQueryState;
 
 public final class SearchWidgetQueryStateTest extends TestCase {
 
   public void testShouldClearDeferredDefaultQueryUpdateForNonDefaultQuery() {
     assertTrue(SearchWidgetQueryState.shouldClearDeferredDefaultQueryUpdate(
-        true, "creator:bob"));
+        true, "creator:bob", "in:inbox"));
   }
 
   public void testShouldNotClearDeferredDefaultQueryUpdateForDefaultQuery() {
     assertFalse(SearchWidgetQueryState.shouldClearDeferredDefaultQueryUpdate(
-        true, "in:inbox"));
+        true, "in:inbox", "in:inbox"));
   }
 }
